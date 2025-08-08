1ec9229fbfdd044716c8b9d5d64b1450a115be73
peterkyle01 <kylepeterkoine4@gmail.com>
2025-08-08 20:58:38 +0300

    feat: Add migration for user table enhancements and enum type for user roles

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20250808_070725 from './20250808_070725';
+import * as migration_20250808_175826 from './20250808_175826';
 
 export const migrations = [
   {
     up: migration_20250808_070725.up,
     down: migration_20250808_070725.down,
-    name: '20250808_070725'
+    name: '20250808_070725',
+  },
+  {
+    up: migration_20250808_175826.up,
+    down: migration_20250808_175826.down,
+    name: '20250808_175826'
   },
 ];
